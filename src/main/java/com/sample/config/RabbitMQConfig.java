7685c08d77fdec6dfe3c3f6f27244e2b9f0dee20
MonishaaMahe <monishaa.sampath@ideas2it.com>
2025-05-12 16:03:49 +0530

    config file change

--- src/main/java/com/sample/config/RabbitMQConfig.java
+++ src/main/java/com/sample/config/RabbitMQConfig.java
@@ -4,6 +4,10 @@ import org.springframework.amqp.core.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 @Configuration
 public class RabbitMQConfig {
@@ -13,6 +17,19 @@ public class RabbitMQConfig {
 
     @Value("${rabbitmq.routing-key}")
     private String routingKey;
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+
 
     @Bean
     public DirectExchange patientExchange() {
